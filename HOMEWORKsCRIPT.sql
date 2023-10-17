47167cdde87c4ec72a415083121db24ec1493ffe
dylan-dot-c <78489976+dylan-dot-c@users.noreply.github.com>
2023-10-17 11:55:28 -0400

    Update HOMEWORKsCRIPT.sql

--- HOMEWORKsCRIPT.sql
+++ HOMEWORKsCRIPT.sql
@@ -4,7 +4,7 @@ select COUNT(*) from actor where last_name = 'Wahlberg';
 
 --2. How many payments were made between $3.99 and $5.99?
 -- ANSWER: 5607
-select COUNT(*) from payment where amount between 3.99 AND 5.99;
+select COUNT(*) from payment where amount between 3.99 AND 5.99; --all inclusive
 
 --3. What films have exactly 7 copies? (search in inventory)
 -- ANSWER: 116 is the total amount...
@@ -23,10 +23,13 @@ select count(distinct  district) from address group by district;
 --
 --7. What film has the most actors in it? (use film_actor table and get film_id)
 -- film id 508
-select film_id, count(actor_id) as num from film_actor group by film_id order by num desc limit 1 ;
+select film_id, count(actor_id) as num 
+from film_actor 
+group by film_id order by num desc limit 1 ;
 --8. From store_id 1, how many customers have a last name ending with ‘es’? (use customer table)
 -- ANS is 21
-select count(last_name) from customer where last_name like '%es' ;
+select count(last_name) 
+from customer where last_name like '%es' and store_id = 1 ;
 --9. How many payment amounts (4.99, 5.99, etc.) had a number of rentals above 250 for customers
 --with ids between 380 and 430? (use group by and having > 250)
 -- ANSWER = 3
@@ -37,7 +40,9 @@ customer_id between 380 and 430 group by amount  having count(amount) > 250 ;
 --10. Within the film table, how many rating categories are there? And what rating has the most
 --movies total?
 -- There are 5 rating CATEGORIES
-select distinct rating from film;
+select rating, count(*) as number from film group by rating order by number DESC;
 -- 
-select count(film_id) as num, category_id  from film_category group by category_id;
+--
+--select rating, count(*) yy
+--select count(film_id) as num, category_id  from film_category group by category_id;
 -- ANSWER: CATEGORY 15
